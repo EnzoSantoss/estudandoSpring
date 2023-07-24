d08f9adfa8863dee711023d6c927c9414bad1649
EnzoSantoss <enzoaugusto312@gmail.com>
2023-07-24 20:13:28 -0300

    first commit

---
+++ src/main/java/com/meuProjeto/enzoSpring/EnzoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.meuProjeto.enzoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnzoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnzoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meuProjeto/enzoSpring/EnzoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuProjeto.enzoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnzoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
